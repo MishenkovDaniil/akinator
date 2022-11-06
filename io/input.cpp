b0e1be195a691a63d8a6e2422591bfc7ef55f0dc
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-06 23:18:54 +0300

    update function

--- io/input.cpp
+++ io/input.cpp
@@ -7,13 +7,12 @@
 #include "../tree/tree.h"
 #include "input.h"
 
-Tnode *tree_fill (Tree *tree, Tnode *node, FILE *file, char *buf)
+Tnode *tree_fill (Tree *tree, Tnode *node, FILE *file, char *buf, int *buf_pos)
 {
-    char symbol = 0;
+    //char symbol = 0;
     char str[100] = {};
     char str_case[100] = {};
     int temp = 0;
-    static int number = 0;
 
     fscanf (file, "%s", str);
     //printf ("{ is [%c]\n", *str);
@@ -22,15 +21,15 @@ Tnode *tree_fill (Tree *tree, Tnode *node, FILE *file, char *buf)
     {
         fscanf (file, "%[^\"]", str_case);
         fgetc (file);
-        fscanf (file, "%[^\"]%n", buf + number, &temp);
+        fscanf (file, "%[^\"]%n", buf + *buf_pos, &temp);
         //printf ("temp is %d\n", temp);
         fgetc (file);
 
-        node->node_case = (const char *)(buf + number);
+        node->node_case = (const char *)(buf + *buf_pos);
 
-        number += temp;
-        *(buf + number) = '\0';
-        number++;
+        *buf_pos += temp;
+        *(buf + *buf_pos) = '\0';
+        (*buf_pos)++;
         temp = 0;
         //printf ("number is %d\n", number);
 
@@ -41,21 +40,20 @@ Tnode *tree_fill (Tree *tree, Tnode *node, FILE *file, char *buf)
         //printf ("next is [%c]\n", *str);
     }
 
-    if (*str == '}')
-    {
-        return node;
-    }
-    else if (*str == '{')
+    if (*str == '{')
     {
         ungetc (*str, file);
 
         add_node (tree, node, nullptr, nullptr);
 
-        tree_fill (tree, node->left, file, buf);
-        tree_fill (tree, node->right, file, buf);
+        tree_fill (tree, node->left, file, buf, buf_pos);
+        tree_fill (tree, node->right, file, buf, buf_pos);
 
         fscanf (file, "%s", str);
+    }
 
+    if (*str == '}')
+    {
         return node;
     }
     else
--- io/input.h
+++ io/input.h
@@ -4,6 +4,6 @@
 #include "../tree_struct.h"
 #include "../tree/tree.h"
 
-Tnode *tree_fill (Tree *tree, Tnode *node, FILE *file, char *buf);
+Tnode *tree_fill (Tree *tree, Tnode *node, FILE *file, char *buf, int *buf_pos);
 
 #endif /* INPUT_H */
