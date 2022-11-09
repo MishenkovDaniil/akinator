4addda2987194457cb0c96020dfcaf9a21d4e60e
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-10 02:24:31 +0300

    made check if tree_info file failed to open

--- io/output.cpp
+++ io/output.cpp
@@ -6,7 +6,7 @@
 
 void tree_print (Tree *tree, const Tnode *node, const char *file)
 {
-    static int tab_num = 1;
+    static int tab_num = 1;                         ///number of tabs to put before case
 
     FILE *tree_info = nullptr;
 
@@ -17,6 +17,12 @@ void tree_print (Tree *tree, const Tnode *node, const char *file)
     else
     {
         tree_info = fopen (file, "a");
+    }
+    assert (tree_info);
+
+    if (tree_info == nullptr)
+    {
+        fprintf (stderr, "fopen failed\n function: %s\nfile: %s", __PRETTY_FUNCTION__, __FILE__);
     }
 
     fprintf (tree_info, "{ \"%s\"", node->node_case);
@@ -31,6 +37,7 @@ void tree_print (Tree *tree, const Tnode *node, const char *file)
         }
 
         int cur_tab_num = tab_num++;
+
         fclose (tree_info);
 
         tree_print (tree, node->left, file);
@@ -41,17 +48,21 @@ void tree_print (Tree *tree, const Tnode *node, const char *file)
         {
             fprintf (tree_info, "\t");
         }
+
         fclose (tree_info);
 
         tree_print (tree, node->right, file);
-
         tab_num--;
+
         tree_info = fopen (file, "a");
+
         for (int i = 1; tab_num - i > 0; i++)
         {
             fprintf (tree_info, "\t");
         }
+
         fprintf (tree_info, "}\n");
+
         fclose (tree_info);
     }
     else
@@ -59,5 +70,36 @@ void tree_print (Tree *tree, const Tnode *node, const char *file)
         fprintf (tree_info, " }\n");
 
         fclose (tree_info);
+    }
+}
+
+void tree_print_mask (Tree *tree, int similar_mask, int mask)
+{
+    Tnode *node = tree->root;
+
+    int mask_shift = 0;
+
+    while (node->right != nullptr || node->left != nullptr)
+    {
+        if ((similar_mask & (0x1 << mask_shift)) == 0)
+        {
+            if ((mask & (0x1 << mask_shift)) == 0)
+            {
+                printf ("doesn't ");
+            }
+
+            printf ("%s, ", node->node_case);
+        }
+
+        if (mask & (0x1 << mask_shift))
+        {
+            node = node->left;
+        }
+        else
+        {
+            node = node->right;
+        }
+
+        mask_shift++;
     }
 }
