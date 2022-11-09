a61388b73b6de2a66356b7e392c2f117023cf5f4
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-10 02:16:24 +0300

    made constant value for str and str case size, delete debug info (debug prints), add checking if tree_fill returned nullptr

--- io/input.cpp
+++ io/input.cpp
@@ -9,35 +9,28 @@
 
 Tnode *tree_fill (Tree *tree, Tnode *node, FILE *file, char *buf, int *buf_pos)
 {
-    //char symbol = 0;
-    char str[100] = {};
-    char str_case[100] = {};
+    const int MAX_STR_LEN = 100;
+
+    char str[MAX_STR_LEN] = {};
+    char str_case[MAX_STR_LEN] = {};
     int temp = 0;
 
     fscanf (file, "%s", str);
-    //printf ("{ is [%c]\n", *str);
 
     if (*str == '{')
     {
-        fscanf (file, "%[^\"]", str_case);
-        fgetc (file);
-        fscanf (file, "%[^\"]%n", buf + *buf_pos, &temp);
-        //printf ("temp is %d\n", temp);
-        fgetc (file);
+        fscanf (file, "%[^\"]", str_case);                  ///reads spaces before first "
+        fgetc (file);                                       ///reads first "
+        fscanf (file, "%[^\"]%n", buf + *buf_pos, &temp);   ///reads case (without "") in buffer
+        fgetc (file);                                       ///reads second "
 
         node->node_case = (const char *)(buf + *buf_pos);
 
         *buf_pos += temp;
         *(buf + *buf_pos) = '\0';
         (*buf_pos)++;
-        temp = 0;
-        //printf ("number is %d\n", number);
-
-        //printf ("str is [%s]\n", node->node_case);
 
         fscanf (file, "%s", str);
-
-        //printf ("next is [%c]\n", *str);
     }
 
     if (*str == '{')
@@ -46,19 +39,20 @@ Tnode *tree_fill (Tree *tree, Tnode *node, FILE *file, char *buf, int *buf_pos)
 
         add_node (tree, node, nullptr, nullptr);
 
-        tree_fill (tree, node->left, file, buf, buf_pos);
-        tree_fill (tree, node->right, file, buf, buf_pos);
+        if (tree_fill (tree, node->left,  file, buf, buf_pos) == nullptr ||
+            tree_fill (tree, node->right, file, buf, buf_pos) == nullptr)
+        {
+            return nullptr;
+        }
 
         fscanf (file, "%s", str);
     }
 
-    if (*str == '}')
-    {
-        return node;
-    }
-    else
+    if (*str != '}')
     {
         fprintf (stderr, "ERROR: syntax error");
+
+        return nullptr;
     }
 
     return node;
