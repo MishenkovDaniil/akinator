02a9519193211ec0850dbaa4ebd45143b37c115a
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-10 01:54:06 +0300

    made define for option, constant value for option max length

--- main.cpp
+++ main.cpp
@@ -1,99 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+//#include "C:\Users\ASUS\Downloads\CodeBlocks\MinGW\x86_64-w64-mingw32\include\sapi.h"
+//#include "C:\Users\ASUS\Downloads\CodeBlocks\MinGW\x86_64-w64-mingw32\include\winapifamily.h"
+//#include <festival.h>
 
 #include "tree_struct.h"
 #include "tree/tree.h"
 #include "io/input.h"
 #include "../standart_functions/io/io.h"
 #include "akinator/akinator.h"
+//#define TX_USE_SPEAK
+//#include "../TX/TXLib.h"
 
-static const char *tree_info = "tree_info.txt";
 
 void print_help_info (void);
 
+
 int main ()
 {
+    const char *tree_info = "tree_info.txt";
+
     FILE *info_file = fopen (tree_info, "r");
 
     int tree_info_size = get_file_size (tree_info);
-    char *buf = (char *)calloc (tree_info_size, sizeof (char));
+    char *buf = (char *)calloc (tree_info_size, sizeof (char));///buffer of "akinator" cases
     assert (buf);
 
-    int buf_pos = 0;
+    int buf_pos = 0; ///free position in buffer of "akinator" cases
 
     Tree tree = {};
     tree_ctor (&tree);
 
     tree_fill (&tree, tree.root, info_file, buf, &buf_pos);
 
+#define run_cmd(full_name, short_name, letter, number, action)  \
+if (stricmp (option, full_name)  == 0 ||                        \
+    stricmp (option, short_name) == 0 ||                        \
+    stricmp (option, letter)     == 0 ||                        \
+    stricmp (option, number)     == 0)                          \
+{                                                               \
+    action                                                      \
+}                                                               \
+else
+
+    const int MAX_OPTION_LEN = 15;
+    char option[MAX_OPTION_LEN] = {};
+
     for (;;)
     {
         printf ("choose game option (help for more info)\n");
 
-        char option[10] = {};
         scanf ("%s", option);
 
         if (stricmp (option, "help") == 0 ||
-            *option == 'h' ||
-            *option == 'H')
+            stricmp (option, "h")    == 0)
         {
             print_help_info ();
             continue;
         }
-
-
-        if (stricmp (option, "aki") == 0 ||
-            stricmp (option, "akinator") == 0 ||
-            *option == 'a' ||
-            *option == 'A' ||
-            *option == '1')
-        {
-            aki_play (&tree, tree.root, info_file, buf, &buf_pos);
-        }
-        else if (stricmp (option, "def") == 0 ||
-                stricmp (option, "definition") == 0 ||
-                *option == 'd' ||
-                *option == 'D' ||
-                *option == '2')
-        {
-            give_definition (&tree);
-        }
-        else if (stricmp (option, "comp") == 0 ||
-                stricmp (option, "compare") == 0||
-                *option == 'c' ||
-                *option == 'C' ||
-                *option == '3')
-        {
-            compare (&tree);
-        }
-        else if (stricmp (option, "show") == 0 ||
-                *option == 's' ||
-                *option == 'S' ||
-                *option == '4')
-        {
-            tree_graph (&tree);
-        }
-        else if (stricmp (option, "voice") == 0 ||
-                *option == 'v' ||
-                *option == 'V' ||
-                *option == '5')
-        {
-            ;
-        }
-        else if (stricmp (option, "quit") == 0 ||
-                *option == 'q' ||
-                *option == 'Q' ||
-                *option == '6')
-        {
-
-            break;
-        }
-        else
+        //run_cmd ("help", "h", "h", "h", {print_help_info (); continue;}) -- what's better??
+        run_cmd ("akinator", "aki", "a", "1", {aki_play (&tree, tree.root, info_file, buf, &buf_pos);})
+        run_cmd ("definition", "def", "d", "2", {give_definition (&tree);})
+        run_cmd ("compare", "comp", "c", "3", {compare (&tree);})
+        run_cmd ("show", "show", "s", "4", {tree_graph (&tree);})
+        run_cmd ("quit", "quit", "q", "5", {break;})
+        /*else*/
         {
             printf ("ERROR: wrong command (%s)", option);
         }
     }
+
+#undef run_cmd
 
     tree_dtor (&tree);
 
@@ -102,6 +80,7 @@ int main ()
 
     return 0;
 }
+
 
 void print_help_info (void)
 {
