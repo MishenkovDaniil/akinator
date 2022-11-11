c2ac235720287db3672e9b3796f07bee9d90444c
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-12 00:00:58 +0300

    fixed bugs with wrong bit mask in find character func, wrong comma writing

--- akinator/akinator.cpp
+++ akinator/akinator.cpp
@@ -19,9 +19,8 @@ void aki_play (Tree *tree, Tnode *node, FILE *file, char *buf, int *buf_pos, con
     assert (buf && buf_pos);
 
     printf ("Is your character %s?\n", node->node_case);
-
-
-    char answer [MAX_ANSWER_SIZE] = {};
+//рекцрсия --> цикл
+    char answer [MAX_ANSWER_SIZE] = "";
     scanf ("%s", answer);
 
     if (stricmp (answer, "yes") == 0 ||
@@ -74,22 +73,20 @@ void add_character (Tree *tree, Tnode *node, FILE *info_file, char *buf, int *bu
 
     printf ("enter character name\n");
 
-    fgetc (stdin);//why works??
-    scanf ("%[^\n]%n", buf + *buf_pos, &temp);
+    scanf ("\n%[^\n]%n", buf + *buf_pos, &temp);
 
     *(buf + *buf_pos + temp) = '\0';
 
     add_node (tree, node, buf + *buf_pos, node->node_case);
 
 
-    printf ("by what does %s differ from %s\n", buf + *buf_pos, node->node_case);//fgets
+    printf ("by what does %s differ from %s\n", buf + *buf_pos, node->node_case);
 
     *buf_pos += temp + 1;
 
     temp = 0;
 
-    fgetc (stdin);//why works??
-    scanf ("%[^\n]%n", buf + *buf_pos, &temp);//fgets
+    scanf ("\n%[^\n]%n", buf + *buf_pos, &temp);
 
     *(buf + *buf_pos + temp) = '\0';
 
@@ -99,7 +96,7 @@ void add_character (Tree *tree, Tnode *node, FILE *info_file, char *buf, int *bu
 
     printf ("do you want to save changes?");
 
-    char answer[MAX_ANSWER_SIZE] = {};
+    char answer[MAX_ANSWER_SIZE] = "";
     scanf ("%s", answer);
 
     if (stricmp (answer, "yes") == 0 || stricmp (answer, "y") == 0)
@@ -112,13 +109,12 @@ void give_definition (Tree *tree)
 {
     assert (tree);
 
-
     printf ("enter character name\n");
 
-    char character[MAX_NAME_LEN] = {};
+    char character[MAX_NAME_LEN] = "";
     scanf ("%s", character);//fgets
 
-    char definition[MAX_DEF_LEN] = {};
+    char definition[MAX_DEF_LEN] = "";
 
     int status = 0;
     find_character (tree, tree->root, character, definition, &status);
@@ -138,19 +134,29 @@ int find_character (Tree *tree, Tnode *node, const char *character, char *defini
     static int number = 0;
     static int mask_shift = 0;
 
+    int mask_shift_now = mask_shift;
+    int mask_now = *mask;
+    int num_now = number;
+    int comma_size = 0;
     int temp = 0;
 
     if (stricmp (node->node_case, character) == 0)
     {
-        *(definition + number - 2) = '\0';
+        *(definition + number) = '\0';
         number = 0;
 
         return *mask;
     }
     else if (node->left != nullptr && node->right != nullptr)
     {
-        temp = sprintf (definition + number, "%s, ", node->node_case);
-        number += temp;
+        if (mask_shift)
+        {
+            sprintf (definition + number, ", ");
+            comma_size = 2;
+        }
+
+        temp = sprintf (definition + comma_size + number, "%s", node->node_case);
+        number += temp + comma_size;
 
         *mask |= 0x1 << mask_shift;
         mask_shift++;
@@ -161,17 +167,15 @@ int find_character (Tree *tree, Tnode *node, const char *character, char *defini
             number = 0;
 
             mask_shift = 0;
+            printf ("[%d]", *mask);
             return *mask;
         }
 
-        mask_shift--;
+        mask_shift = mask_shift_now;
+        *mask = mask_now;
 
-        number -= temp;
-
-        temp = sprintf (definition + number, "doesn't %s, ", node->node_case);
-        number += temp;
-
-        *mask &= ~(0x1 << mask_shift);
+        temp = sprintf (definition + comma_size + num_now, "doesn't %s", node->node_case);
+        number = num_now + temp + comma_size;
 
         mask_shift++;
 
@@ -181,10 +185,12 @@ int find_character (Tree *tree, Tnode *node, const char *character, char *defini
             number = 0;
 
             mask_shift = 0;
+            printf ("[%d]", *mask);
+
             return *mask;
         }
 
-        number -= temp;
+        number -= temp + comma_size;
     }
 
     return 0;
@@ -193,21 +199,21 @@ int find_character (Tree *tree, Tnode *node, const char *character, char *defini
 void compare (Tree *tree)
 {
     printf ("enter first character to compare\n");
-    char first_character[MAX_NAME_LEN] = {};
+    char first_character[MAX_NAME_LEN] = "";
     scanf ("%s", first_character);
 
     printf ("enter second character to compare\n");
-    char second_character[MAX_NAME_LEN] = {};
+    char second_character[MAX_NAME_LEN] = "";
     scanf ("%s", second_character);
 
-    char definition_1[MAX_DEF_LEN] = {};
-    char definition_2[MAX_DEF_LEN] = {};
+    char definition_1[MAX_DEF_LEN] = "";
+    char definition_2[MAX_DEF_LEN] = "";
 
     int status_1 = 0;
     find_character (tree, tree->root, first_character, definition_1, &status_1);
     int status_2 = 0;
     find_character (tree, tree->root, second_character, definition_2, &status_2);
-
+    printf ("%d, %d\n", status_1, status_2);
     int differ_mask = 0;
     int similar_mask = 0;
     int mask_shift = 0;
@@ -218,38 +224,49 @@ void compare (Tree *tree)
     {
         int search = (status_1 > status_2) ? status_1 : status_2;
 
-        printf ("%s and %s are both ", first_character, second_character);
+        printf ("%s and %s ", first_character, second_character);
 
-        while (search > 0 && node != nullptr)
+        while (search > 0 && node != nullptr && (status_1 & (0x1 << mask_shift)) == (status_2 & (0x1 << mask_shift)))
         {
-            if ((status_1 & (0x1 << mask_shift)) == (status_2 & (0x1 << mask_shift)))
+            if (!(mask_shift))
             {
-                if ((status_1 & (0x1 << mask_shift)) == 1)
-                {
-                    printf ("%s, ", node->node_case);
-
-                    node = node->left;
-                }
-                else
-                {
-                    printf ("doesn't %s, ", node->node_case);
-
-                    node = node->right;
-                }
-
-                similar_mask |= 0x1 << mask_shift;
+                printf ("are both ");
             }
+            else
+            {
+                printf (", ");
+            }
+
+            if ((status_1 & (0x1 << mask_shift)) == 0)
+            {
+                printf ("doesn't %s", node->node_case);
+
+                node = node->right;
+            }
+            else
+            {
+                printf ("%s", node->node_case);
+
+                node = node->left;
+            }
+
+            similar_mask |= 0x1 << mask_shift;
 
             search /= 2;
             mask_shift++;
         }
 
-        printf ("\nbut %s ", first_character);
+        if (!(similar_mask))
+        {
+            printf ("have nothing in similar");
+        }
+
+        printf (",\nbut %s ", first_character);
         tree_print_mask (tree, similar_mask, status_1);
 
-        printf ("\nand %s ", second_character);
+        printf (",\nand %s ", second_character);
         tree_print_mask (tree, similar_mask, status_2);
-        printf ("\n");
+        printf (".\n");
     }
     else
     {
