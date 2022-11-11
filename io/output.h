9ddbb4ed1091e755503b9e785bc65fbafa7ebc20
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-12 00:02:01 +0300

    made mask parameters const

--- io/output.h
+++ io/output.h
@@ -2,5 +2,6 @@
 #define OUTPUT_H
 
 void tree_print (Tree *tree, const Tnode *node, const char *file);
+void tree_print_mask (Tree *tree, const int similar_mask, const int mask);
 
 #endif /* OUTPUT_H */
