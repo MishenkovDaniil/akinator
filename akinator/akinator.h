10306cb2a1756948fd5483a56253bd8223a0a1cb
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-14 02:52:38 +0300

    update prototypes for functions using stack, made new ones for functions is_continue and checking answer functions

--- akinator/akinator.h
+++ akinator/akinator.h
@@ -1,13 +1,15 @@
 #ifndef AKINATOR_H
 #define AKINATOR_H
 
-#include "../tree_struct.h"
 #include "../tree/tree.h"
 
-void aki_play (Tree *tree, Tnode *node, FILE *file, char *buf, int *buf_pos, const char *tree_info_file);
-void add_character (Tree *tree, Tnode *node, FILE *info_file, char *buf, int *buf_pos, const char *tree_info_file);
+int is_yes_answer (const char *answer);
+int is_no_answer (const char *answer);
+int is_continue (char *input);
+
+void aki_play (Tree *tree, Node *node, FILE *file, char *buf, int *buf_pos, const char *tree_info_file);
+void add_character (Tree *tree, Node *node, FILE *info_file, char *buf, int *buf_pos, const char *tree_info_file);
 void give_definition (Tree *tree);
-int find_character (Tree *tree, Tnode *node, const char *character, char *definition, int *mask);
 void compare (Tree *tree);
 
 #endif /* AKINATOR_H */
