8281f5d12640e72c011f0b2ee74cbb6f947b3aa5
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-14 01:47:41 +0300

    add new function' declarations, delete unused ones, moved struct declaration from tree_struct.h

--- tree/tree.h
+++ tree/tree.h
@@ -1,20 +1,30 @@
 #ifndef TREE_H
 #define TREE_H
 
-#include "../tree_struct.h"
+struct Node
+{
+    const char *data = nullptr;
+    Node *left  = nullptr;
+    Node *right = nullptr;
+};
+
+struct Tree
+{
+    Node *root = nullptr;//add debug info
+};
 
 static unsigned int ERRNO = 0;
 
+Node *tree_create_node (const char *data = nullptr);
+
 void tree_ctor (Tree *tree, unsigned int *err = &ERRNO);
-static Tnode *talloc (void);
-Tnode *add_left_node  (Tree *tree, Tnode *add_node, const char *left_case,  unsigned int *err = &ERRNO);
-Tnode *add_right_node (Tree *tree, Tnode *add_node, const char *right_case, unsigned int *err = &ERRNO);
-Tnode *add_node (Tree *tree, Tnode *add_node, const char *left_case, const char *right_case, unsigned int *err = &ERRNO);
+void add_node (Tree *tree, Node *parent_node, const char *left_case, const char *right_case, unsigned int *err = &ERRNO);
 void tree_dtor (Tree *tree, unsigned int *err = &ERRNO);
-void tree_free (Tnode *node);
+void tree_free (Node *node);
+
 unsigned int tree_check (Tree *tree, unsigned int *err);
 void tree_dump (Tree *tree, unsigned int *err);
 void tree_graph (Tree *tree);
-int make_graph_nodes (Tnode *node, FILE *tgraph_file);
+int make_graph_nodes (Node *node, FILE *tgraph_file);
 
 #endif /* AKINATOR_H */
