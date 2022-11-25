97edb5f4e694e5ab0d5b86403d1645628887fb52
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-25 13:47:38 +0300

    add error ending of working system voice func

--- io/output.cpp
+++ io/output.cpp
@@ -1,12 +1,46 @@
 #include <stdio.h>
 #include <assert.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 #include "../tree/tree.h"
 #include "output.h"
 
+static const int MAX_OUTPUT_LEN = 500;
+
+int system_voice (int num, ...)
+{
+    char command[MAX_OUTPUT_LEN]  = "";
+
+    va_list str = nullptr;
+
+    va_start (str, num);
+
+    for (int i = 0; i < num; i++)
+    {
+        char *string = va_arg (str, char *);
+        strcat (command, string);
+    }
+
+    if (command[MAX_OUTPUT_LEN] != '\0')
+    {
+        fprintf (stderr, "Error: buffer overflow, function %s in file %s.\n", __PRETTY_FUNCTION__, __FILE__);
+        return false;
+    }
+
+    system (command);
+    *command = '\0';
+
+    va_end (str);
+
+    return true;
+}
+
 void tree_print (Tree *tree, const Node *node, const char *file)
 {
-    static int tab_num = 1;                         ///number of tabs to put before case
+    static int tab_num = 1;                         ///<number of tabs to put before case
 
     FILE *tree_info = nullptr;
 
